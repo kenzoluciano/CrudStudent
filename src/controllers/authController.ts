5d7beb8985c72c8946154df11dc25a03913e0aff
RAKOTOMALALA ADRIANO Luciano Kenzo <kenzoluciano020@gmail.com>
2026-08-20 12:18:43 +0300

    feat: controller Auth

---
+++ src/controllers/authController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import * as authService from "../services/authService";
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = await authService.register(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+    const result = await authService.login(email, password);
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
