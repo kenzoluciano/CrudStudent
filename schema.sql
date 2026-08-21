b29b7e81bed21737f37fa7adfbc3416163f02492
RAKOTOMALALA ADRIANO Luciano Kenzo <kenzoluciano020@gmail.com>
2026-08-21 11:24:27 +0300

    chore: suppression de schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE etudiants (
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    prenom VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    date_naissance DATE,
-    created_at TIMESTAMP DEFAULT NOW()
-);
